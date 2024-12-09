2f65aeb1cf4e52ffdf99b83993c6463f9fb055a2
seoboin <sbi1024@naver.com>
2024-12-09 15:07:55 +0900

    health api 추가

--- src/main/java/com/deploy/api/controller/ApiServerController.java
+++ src/main/java/com/deploy/api/controller/ApiServerController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@ResponseBody
 public class ApiServerController {
     @GetMapping("/")
-    @ResponseBody
     public String apiServerDeployTest() {
         return "api-server 를 정상적으로 EC2환경에서 배포 하였습니다.";
+    }
+
+    @GetMapping("/health")
+    public String apiServerHealthCheck() {
+        return "Success Heatlth Check";
     }
 }
